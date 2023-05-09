0368768604be545fd044957a5ee6d6fbcbcb0dd4
Some-Regular-Person <31082683+Some-Regular-Person@users.noreply.github.com>
2023-05-09 13:55:06 -0700

    Started implementing Book and Item

--- CS3560 Final Project/src/Book.java
+++ CS3560 Final Project/src/Book.java
@@ -1,19 +1,49 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Expression;
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+@Entity
+@Table(name = "books", schema = "library")
+@Inheritance(strategy = InheritanceType.JOINED)
 public class Book extends Item
 {
+	@Column(name = "pages")
 	private int pages;
+	@Column(name = "publisher")
 	private String publisher;
+	@Column(name = "publication_date")
 	private Date publicationDate;
-	private Author author;
+	// TODO finish annotations
+	@ManyToMany
+	@JoinTable(name = "books_authors")
+	private List<Author> authors;
+	
+	public Book() {
+		super(null, null, null, 0);
+	}
 	
 	// constructor
-	public Book(String title, String description, String location, double dailyPrice,
-				int pages, String publisher, Date publicationDate, Author author)
+	public Book(String title, String description, String location, double dailyPrice)
 	{
 		super(title, description, location, dailyPrice);
-		this.pages = pages;
-		this.publisher = publisher;
-		this.publicationDate = publicationDate;
-		this.author = author;
 	}
 
 	// setters
@@ -32,9 +62,12 @@ public class Book extends Item
 		this.publicationDate = publicationDate;
 	}
 	
-	public void setAuthor(Author author)
-	{
-		this.author = author;
+	public void addAuthor(Author author) {
+		authors.add(author);
+	}
+	
+	public void removeAuthor(Author author) {
+		authors.remove(author);
 	}
 
 	// getters
@@ -53,18 +86,117 @@ public class Book extends Item
 		return publicationDate;
 	}
 
-	public Author getAuthor()
+	public List<Author> getAuthors()
 	{
-		return author;
+		List<Author> result = new ArrayList<Author>();
+		Collections.copy(result, authors);
+		return result;
 	}
 	
 	// overriding toString()
 	@Override
 	public String toString()
 	{
-		return author + "Page count: " + pages + "\nPublisher: " + publisher +
-			   "\nPublication Date: " + publicationDate + super.toString();
+		String result = "Page count: " + pages + "\nPublisher: " + publisher +
+				   	    "\nPublication Date: " + publicationDate + super.toString() +
+				   	    "\nAuthor(s):";
+		
+		for (Author author : authors) {
+			result += "\n" + author.toString();
+		}
+		
+		return result;
 	}
 	
-	// TODO implement findBy() here
+	public static List<Book> findBy(BookQuery bookQuery) {
+		SessionFactory sessionFactory = HibernateSessionFactory.getSessionFactory();
+		Session session = sessionFactory.getCurrentSession();
+		
+		session.beginTransaction();
+		
+		CriteriaBuilder cb = session.getCriteriaBuilder();
+		CriteriaQuery<Book> query = cb.createQuery(Book.class);
+		Root<Book> root = query.from(Book.class);
+		Join<Book, Author> joined = root.join("authors");
+		
+		List<Predicate> predicates = new ArrayList<Predicate>();
+		
+		if (bookQuery.getCode() != null) {
+			predicates.add(cb.equal(joined.get("code"), cb.parameter(String.class, "code")));
+		}
+		
+		if (bookQuery.getTitle() != null) {
+			predicates.add(cb.like(joined.get("title"), cb.parameter(String.class, "title")));
+		}
+		
+		if (bookQuery.getLocation() != null) {
+			predicates.add(cb.like(joined.get("location"), cb.parameter(String.class, "location")));
+		}
+		
+		if (bookQuery.getMaxDailyPrice() != null) {
+			predicates.add(cb.lessThanOrEqualTo(joined.get("daily_price"), bookQuery.getMaxDailyPrice()));
+		}
+		
+		if (bookQuery.getMinDailyPrice() != null) {
+			predicates.add(cb.greaterThanOrEqualTo(joined.get("daily_price"), bookQuery.getMinDailyPrice()));
+		}
+		
+		if (bookQuery.isOnlyAvailable()) {
+			predicates.add(cb.equal(joined.get("available"), true));
+		}
+		
+		if (bookQuery.getMaxPages() != null) {
+			predicates.add(cb.lessThanOrEqualTo(joined.get("pages"), bookQuery.getMaxPages()));
+		}
+		
+		if (bookQuery.getMinPages() != null) {
+			predicates.add(cb.greaterThanOrEqualTo(joined.get("pages"), bookQuery.getMinPages()));
+		}
+		
+		// TODO implement logic for published after and published before
+		if (bookQuery.getPublishedAfter() != null) {
+			predicates.add(cb.greaterThanOrEqualTo(joined.get("publication_date"), bookQuery.getPublishedAfter()));
+		}
+		
+		if (bookQuery.getPublisher() != null) {
+			predicates.add(cb.like(joined.get("publisher"), cb.parameter(String.class, "publisher")));
+		}
+		
+		if (bookQuery.getAuthorName() != null) {
+			predicates.add(cb.like(joined.get("name"), cb.parameter(String.class, "authorName")));
+		}
+		
+		if (predicates.size() > 0) 
+			query = query.where(cb.and(predicates.toArray(new Predicate[0])));
+		
+		TypedQuery<Book> typedQuery = session.createQuery(query);
+		
+		if (bookQuery.getCode() != null) {
+			typedQuery.setParameter("code", bookQuery.getCode());
+		}
+		
+		if (bookQuery.getTitle() != null) {
+			typedQuery.setParameter("title", bookQuery.getTitle());
+		}
+		
+		if (bookQuery.getLocation() != null) {
+			typedQuery.setParameter("location", bookQuery.getLocation());
+		}
+		
+		if (bookQuery.getPublisher() != null) {
+			typedQuery.setParameter("publisher", bookQuery.getPublisher());
+		}
+		
+		if (bookQuery.getAuthorName() != null) {
+			typedQuery.setParameter("authorName", bookQuery.getAuthorName());
+		}
+		
+		List<Book> books = session.createQuery(query).getResultList();
+		
+		session.getTransaction().commit();
+		
+		return books;
+	}
+	
+	
 }
--- CS3560 Final Project/src/Item.java
+++ CS3560 Final Project/src/Item.java
@@ -1,10 +1,29 @@
-public class Item
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.criteria.Predicate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+@MappedSuperclass
+public abstract class Item implements CRUDOperations
 {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "code")
 	private int code;
+	@Column(name = "available")
 	private boolean available;
+	@Column(name = "title")
 	private String title;
+	@Column(name = "description")
 	private String description;
+	@Column(name = "location")
 	private String location;
+	@Column(name = "daily_price")
 	private double dailyPrice;
 	
 	// constructor
@@ -93,5 +112,39 @@ public class Item
 				location + "\nDaily price: $" + formatPrice + "\nAvailability: " + msg + "\n";
 	}
 	
-	// TODO implement makeLoanTo();
+	// TODO implement makeLoanTo
+	public Loan makeLoanTo(Student student) {
+		return null;
+	}
+	
+	@Override
+	public void create() {
+		SessionFactory sessionFactory = HibernateSessionFactory.getSessionFactory();
+		Session session = sessionFactory.getCurrentSession();
+		
+		session.beginTransaction();
+		session.save(this);
+		session.getTransaction().commit();
+	}
+
+	@Override
+	public void update() {
+		SessionFactory sessionFactory = HibernateSessionFactory.getSessionFactory();
+		Session session = sessionFactory.getCurrentSession();
+		
+		session.beginTransaction();
+		session.update(this);
+		session.getTransaction().commit();
+	}
+
+	@Override
+	public void delete() {
+		SessionFactory sessionFactory = HibernateSessionFactory.getSessionFactory();
+		Session session = sessionFactory.getCurrentSession();
+		
+		session.beginTransaction();
+		session.delete(this);
+		session.getTransaction().commit();
+	}
+	
 }
