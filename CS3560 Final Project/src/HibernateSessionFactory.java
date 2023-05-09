a8faa216830b2553aad7a3446fcd1fa708685ad6
Some-Regular-Person <31082683+Some-Regular-Person@users.noreply.github.com>
2023-05-08 22:21:27 -0700

    Created SessionFactory singleton

---
+++ CS3560 Final Project/src/HibernateSessionFactory.java
@@ -0,0 +1,22 @@
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateSessionFactory {
+	
+	private static SessionFactory sessionFactory;
+	
+	public static SessionFactory getSessionFactory() {
+		if (sessionFactory == null) {
+			sessionFactory = new Configuration()
+								 .configure("hibernate.cfg.xml")
+				                 .addAnnotatedClass(Person.class)
+				                 .addAnnotatedClass(Author.class)
+				                 .addAnnotatedClass(Director.class)
+				                 .addAnnotatedClass(Student.class)
+				                 .buildSessionFactory();
+		}
+		
+		return sessionFactory;
+	}
+	
+}
