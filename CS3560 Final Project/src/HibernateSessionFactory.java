aa86620caf0508fc748beaa86d5d8a820df54823
Some-Regular-Person <31082683+Some-Regular-Person@users.noreply.github.com>
2023-05-10 14:32:34 -0700

    Make session factory multithreading safe

--- CS3560 Final Project/src/HibernateSessionFactory.java
+++ CS3560 Final Project/src/HibernateSessionFactory.java
@@ -5,7 +5,9 @@ public class HibernateSessionFactory {
 	
 	private static SessionFactory sessionFactory;
 	
-	public static SessionFactory getSessionFactory() {
+	private HibernateSessionFactory() {}
+	
+	public synchronized static SessionFactory getSessionFactory() {
 		if (sessionFactory == null) {
 			sessionFactory = new Configuration()
 								 .configure("hibernate.cfg.xml")
