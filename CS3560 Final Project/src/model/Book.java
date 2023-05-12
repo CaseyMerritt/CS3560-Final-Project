5a75c1cbc5eaaabc4d10909bfd65f1f42df8b81a
Some-Regular-Person <31082683+Some-Regular-Person@users.noreply.github.com>
2023-05-11 17:56:34 -0700

    Update date imports

--- CS3560 Final Project/src/model/Book.java
+++ CS3560 Final Project/src/model/Book.java
@@ -2,8 +2,7 @@ package model;
 
 import java.util.ArrayList;
 import java.util.Collections;
-//import java.util.Date;
-//import java.sql.Date;
+import java.util.Date;
 import java.util.List;
 
 import javax.persistence.Column;
--- CS3560 Final Project/src/model/Film.java
+++ CS3560 Final Project/src/model/Film.java
@@ -1,8 +1,7 @@
 package model;
 
 import java.util.ArrayList;
-//import java.util.Date;
-//import java.sql.Date;
+import java.util.Date;
 import java.util.List;
 
 import javax.persistence.Column;
